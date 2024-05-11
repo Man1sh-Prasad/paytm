649bc5af6209a4bcbfe05109a4fa3165be320c1f
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-05-12 01:35:30 +0530

    add update schema

--- backend/src/types.js
+++ backend/src/types.js
@@ -11,3 +11,9 @@ export const signinSchema = z.object({
     username: z.string().email(),
     password: z.string().min(8)
 })
+
+export const updateSchema = z.object({
+    firstname: z.string().optional(),
+    lastname: z.string().optional(),
+    password: z.string().min(8).optional()
+})
