3472be340d43f5e9f968645d8c448f85fd136020
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-05-15 02:22:37 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import './App.css'
+import { Signup } from './pages/Signup'
+import { Dashboard } from './pages/Dashboard'
+import { Signin } from './pages/Signin'
+import { SendMoney } from './pages/SendMoney'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/send' element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
