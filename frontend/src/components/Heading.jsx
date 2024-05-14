1f2ec9ae71a5765712bf39f747c7a46c180a21a2
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-05-15 02:39:40 +0530

    fix heading positioning (to center)

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -1,5 +1,5 @@
 export function Heading({label}) {
-    return <div className="font-bold text-3xl pt-6 pb-2">
+    return <div className="font-bold text-center text-3xl pt-6 pb-2">
         {label}
     </div>
 }
