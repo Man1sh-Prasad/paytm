26a20a29944a847b7953c46f82b55d7c5bcffe16
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-05-16 03:43:22 +0530

    change in button style

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,6 +1,6 @@
 export function Button({label}) {
     return <div>
-        <button className="w-full bg-black text-white text-xl mt-2 mb-2 px-2 py-1 rounded">
+        <button className="w-full bg-black text-white text-md mt-2 mb-2 px-4 py-1 rounded">
             {label}
         </button>
     </div>
