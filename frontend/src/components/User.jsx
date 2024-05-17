0acb70e4779a22b4ec999ac383e44a3fd5d535c8
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-05-18 02:47:09 +0530

    connected button to sendmoney page

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -1,6 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { Button } from "./Button";
 
-export function User({user}) {
+export function User({user, userId}) {
+    const navigate = useNavigate();
+
+    const handleTransaction = () => {
+        navigate('/send?id=' + user._id + "&name=" + user.firstname)
+    }
+
     return <div className="flex justify-between ml-4 mr-4">
         <div className="flex">
             <div className="h-12 w-12 rounded-full bg-slate-200 flex justify-center mt-1 mr-2">
@@ -9,11 +16,17 @@ export function User({user}) {
                 </div>
             </div>
             <div className="flex flex-col justify-center">
-                {user.firstname} {user.lastname}
+                <div>
+                    {user.firstname} {user.lastname}
+                </div>  
+                <div className="text-sm text-slate-500">
+                    {user.username}
+                </div>
             </div>
+            
         </div>
         <div className="fflex flex-col justify-center h-full">
-            <Button label={"Send Money"} />
+            <Button label={"Send Money"} onClick={handleTransaction} />
         </div>
     </div>
 }
