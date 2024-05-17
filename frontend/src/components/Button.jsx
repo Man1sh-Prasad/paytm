07553f80ad7209532149ad2821ee15824c98040b
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-05-18 02:45:17 +0530

    add onclick functionality

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,6 +1,6 @@
-export function Button({label}) {
+export function Button({label, onClick}) {
     return <div>
-        <button className="w-full bg-black text-white text-md mt-2 mb-2 px-4 py-1 rounded">
+        <button onClick={onClick} className="w-full bg-black text-white text-md mt-2 mb-2 px-4 py-1 rounded">
             {label}
         </button>
     </div>
