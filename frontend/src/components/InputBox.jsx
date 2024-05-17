70b092e2ac450d75b18f16840b91db5cd52afc3d
Man1sh-Prasad <prasadmanish467@gmail.com>
2024-05-18 02:45:39 +0530

    add onchange

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,10 +1,10 @@
-export function InputBox({placeholder, label}) {
+export function InputBox({placeholder, label, onChange}) {
     return <div className="pb-2">
         <div className="font-medium text-left text-sm pb-1">
             {label}
         </div>
         <div className="">
-            <input type="text" placeholder={placeholder} className="text-slate-400 text-sm border-slate-300 border rounded w-full px-2 py-1"/>
+            <input type="text" placeholder={placeholder} onChange={onChange} className="text-slate-400 text-sm border-slate-300 border rounded w-full px-2 py-1"/>
         </div>
     </div>
 }
